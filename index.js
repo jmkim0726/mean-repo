efd25e9531b7cf80e8eeda2aacc51f1998083ee5
jmkim0726 <jm0726.kim@gmail.com>
2017-01-30 22:07:58 +0900

    init ver0.4

--- index.js
+++ index.js
@@ -3,7 +3,19 @@
 var express = require('express');
 var app = express();
 
+// 1
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
+
+// 2
+app.get("/hello", function(req, res) {
+    res.render("hello", {name:req.query.nameQuery});
+    });
+
+// 3
+app.get("/hello/:nameParam", function(req, res) {
+    res.render("hello", {name:req.params.nameParam});
+    });
 
 app.listen(3000, function() {
     console.log('Server On!');
